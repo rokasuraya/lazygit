88caf845666345b74d845d97481d5facf671db93
Rokas Uraya <rokasuraya@users.noreply.github.com>
2026-04-16 04:17:10 +0800

    feat: add main entry point for lazygit

--- main.go
+++ main.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jesseduffield/lazygit/pkg/app"
+	"github.com/jesseduffield/lazygit/pkg/app/daemon"
+	"github.com/jesseduffield/lazygit/pkg/config"
+	"github.com/jesseduffield/lazygit/pkg/env"
+	"github.com/jesseduffield/lazygit/pkg/logs"
 )
 
-// These values may be set by the build script via the LDFLAGS argument
 var (
-	commit      string
-	date        string
-	version     string
-	buildSource = "unknown"
+	// Version is the current version of lazygit, set at build time via ldflags.
+	version = "development"
+	// buildDate is the date the binary was built, set at build time via ldflags.
+	buildDate = "unknown"
+	// commitSHA is the git commit SHA the binary was built from, set at build time via ldflags.
+	commitSHA = "unknown"
 )
 
 func main() {
-	ldFlagsBuildInfo := &app.BuildInfo{
-		Commit:      commit,
-		Date:        date,
-		Version:     version,
-		BuildSource: buildSource,
+	// If we're being run as a daemon (e.g. for a background git operation),
+	// handle that case and exit early.
+	if daemon.TryRunningAsDaemon() {
+		os.Exit(0)
 	}
 
-	app.Start(ldFlagsBuildInfo, nil)
+	buildInfo := &config.BuildInfo{
+		Version:   version,
+		BuildDate: buildDate,
+		Commit:    commitSHA,
+	}
+
+	appConfig, err := config.NewAppConfig(
+		"lazygit",
+		version,
+		commitSHA,
+		buildDate,
+		"https://github.com/jesseduffield/lazygit",
+		env.GetGitDirEnv() != "",
+		buildInfo,
+	)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error building app config: %v", err))
+	}
+
+	logger := logs.Global.WithField("version", version)
+
+	lazygitApp, err := app.NewApp(appConfig, logger)
+	if err != nil {
+		if lazygitApp != nil {
+			_ = lazygitApp.Close()
+		}
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := lazygitApp.Close(); err != nil {
+			log.Printf("Error closing app: %v", err)
+		}
+	}()
+
+	if err := lazygitApp.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
